33b05a02c4d53584c16e85cfc90e3735ca836d04
Bo Hu <hubo611@foxmail.com>
2025-03-27 18:08:44 +0800

    add machine evnet

---
+++ EAP.Client/Secs/PrimaryMessageHandler/EventHandler/01.ClampRequest.cs
@@ -0,0 +1,48 @@
+﻿using EAP.Client.Secs.Models;
+using log4net;
+using Secs4Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Secs4Net.Item;
+
+namespace EAP.Client.Secs.PrimaryMessageHandler.EventHandler
+{
+    class ClampRequest : IEventHandler
+    {
+        internal readonly ILog traLog = LogManager.GetLogger("Trace");
+
+        internal readonly ISecsGem secsGem;
+
+        public ClampRequest(SecsGem secsGem)
+        {
+            this.secsGem = secsGem;
+        }
+
+        public async Task HandleEvent(GemCeid ceid, PrimaryMessageWrapper wrapper)
+        {
+            traLog.Info($"1.Clamp Start.");
+            uint portid = wrapper.PrimaryMessage.SecsItem[2][0][1][0].FirstValue<uint>();
+            var s2f41G003 = new SecsMessage(2, 41)
+            {
+                SecsItem = L(
+                    A("G003"),
+                    L(
+                        L(
+                            A("CLAMP"),
+                            U4(1)
+                        ),
+                         L(
+                            A("PORT-ID"),
+                            U4(portid)
+                        )
+                    )
+
+                )
+            };
+            await secsGem.SendAsync(s2f41G003);
+        }
+    }
+}
---
+++ EAP.Client/Secs/PrimaryMessageHandler/EventHandler/02.CassetteIdReport.cs
@@ -0,0 +1,51 @@
+﻿using EAP.Client.Secs.Models;
+using EAP.Client.Sfis;
+using log4net;
+using Microsoft.Extensions.Configuration;
+using Secs4Net;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace EAP.Client.Secs.PrimaryMessageHandler.EventHandler
+{
+    class CassetteIdReport : IEventHandler
+    {
+        internal readonly ILog traLog = LogManager.GetLogger("Trace");
+
+        internal readonly ISecsGem secsGem;
+        internal readonly IConfiguration configuration;
+        public static string nextLot = string.Empty;
+        public CassetteIdReport(SecsGem secsGem, IConfiguration configuration)
+        {
+            this.secsGem = secsGem;
+            this.configuration = configuration;
+        }
+
+        public Task HandleEvent(GemCeid ceid, PrimaryMessageWrapper wrapper)
+        {
+            traLog.Info($"1.Clamp Success.");
+            traLog.Info($"2.Lotid Check Start.");
+            uint portid = wrapper.PrimaryMessage.SecsItem[2][0][1][0].FirstValue<uint>();
+            string nextLot = wrapper.PrimaryMessage.SecsItem[2][0][1][1].GetString();
+
+            var eqid = configuration.GetSection("Custom")["EquipmentId"];
+            var baymaxIp = configuration.GetSection("Custom")["SfisIp"];
+            var baymaxPort = int.Parse(configuration.GetSection("Custom")["SfisPort"] ?? "21347");
+            string sfis_step1_req = $"{eqid},{nextLot},1,M068397,JORDAN,,OK,";
+            BaymaxService service = new BaymaxService();
+            var trans = service.GetBaymaxTrans(baymaxIp, baymaxPort, sfis_step1_req);
+            if (trans.Result)
+            {
+
+            }
+            else
+            { 
+            }
+        }
+    }
+}
