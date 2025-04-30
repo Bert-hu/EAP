4507077634e840dd87de5b6d6f55a5c6b5c1c118
Bo Hu <hubo611@foxmail.com>
2025-04-30 16:07:16 +0800

    update compare stratagy

--- EAP.Client/Model/AsymtekUnderfill.cs
+++ EAP.Client/Model/AsymtekUnderfill.cs
@@ -29,31 +29,104 @@ namespace EAP.Client.Model
         {
             var differences = new List<string>();
 
-            // 比较 RecipePara 的基本属性
-            differences.AddRange(CompareBasicProperties(other));
+            // 比较 PreheatTime
+            if (Math.Abs(this.PreheatTime - other.PreheatTime) > 20)
+            {
+                differences.Add($"PreheatTime: {this.PreheatTime} != {other.PreheatTime}");
+            }
+
+            // 比较 PreheatTemp
+            if (Math.Abs(this.PreheatTemp - other.PreheatTemp) > 20)
+            {
+                differences.Add($"PreheatTemp: {this.PreheatTemp} != {other.PreheatTemp}");
+            }
+
+            // 比较 NozzleTemp
+            if (Math.Abs(this.NozzleTemp - other.NozzleTemp) > 5)
+            {
+                differences.Add($"NozzleTemp: {this.NozzleTemp} != {other.NozzleTemp}");
+            }
+
+            // 比较 HeatTemp
+            if (Math.Abs(this.HeatTemp - other.HeatTemp) > 20)
+            {
+                differences.Add($"HeatTemp: {this.HeatTemp} != {other.HeatTemp}");
+            }
 
             // 比较 GlueConfigs
-            var glueConfigDiffs = CompareGlueConfigs(other.GlueConfigs);
-            differences.AddRange(glueConfigDiffs);
+            if (this.GlueConfigs != null && other.GlueConfigs != null)
+            {
+                for (int i = 0; i < Math.Min(this.GlueConfigs.Count, other.GlueConfigs.Count); i++)
+                {
+                    var thisGlue = this.GlueConfigs[i];
+                    var otherGlue = other.GlueConfigs[i];
+
+                    // 比较 StartX
+                    if (Math.Abs(thisGlue.StartX - otherGlue.StartX) > 200)
+                    {
+                        differences.Add($"GlueConfig[{i}].StartX: {thisGlue.StartX} != {otherGlue.StartX}");
+                    }
+
+                    // 比较 StartY
+                    if (Math.Abs(thisGlue.StartY - otherGlue.StartY) > 200)
+                    {
+                        differences.Add($"GlueConfig[{i}].StartY: {thisGlue.StartY} != {otherGlue.StartY}");
+                    }
+
+                    // 比较 EndX
+                    if (Math.Abs(thisGlue.EndX - otherGlue.EndX) > 200)
+                    {
+                        differences.Add($"GlueConfig[{i}].EndX: {thisGlue.EndX} != {otherGlue.EndX}");
+                    }
+
+                    // 比较 EndY
+                    if (Math.Abs(thisGlue.EndY - otherGlue.EndY) > 200)
+                    {
+                        differences.Add($"GlueConfig[{i}].EndY: {thisGlue.EndY} != {otherGlue.EndY}");
+                    }
+
+                    // 比较 Weight
+                    double weightDiff = Math.Abs(thisGlue.Weight - otherGlue.Weight);
+                    double weightThreshold = thisGlue.Weight * 0.03;
+                    if (weightDiff > weightThreshold)
+                    {
+                        differences.Add($"GlueConfig[{i}].Weight: {thisGlue.Weight} != {otherGlue.Weight}");
+                    }
+                }
+            }
 
             return differences.Count == 0 ? string.Empty : string.Join(Environment.NewLine, differences);
         }
 
-        private IEnumerable<string> CompareBasicProperties(RecipePara other)
-        {
-            var properties = GetType().GetProperties()
-               .Where(p => p.PropertyType != typeof(List<GlueConfig>));
+        //public string CompareTo1(RecipePara other)
+        //{
+        //    var differences = new List<string>();
 
-            foreach (var property in properties)
-            {
-                var value1 = property.GetValue(this);
-                var value2 = property.GetValue(other);
-                if (!Equals(value1, value2))
-                {
-                    yield return $"{property.Name}: {value1} != {value2}";
-                }
-            }
-        }
+        //    // 比较 RecipePara 的基本属性
+        //    differences.AddRange(CompareBasicProperties(other));
+
+        //    // 比较 GlueConfigs
+        //    var glueConfigDiffs = CompareGlueConfigs(other.GlueConfigs);
+        //    differences.AddRange(glueConfigDiffs);
+
+        //    return differences.Count == 0 ? string.Empty : string.Join(Environment.NewLine, differences);
+        //}
+
+        //private IEnumerable<string> CompareBasicProperties(RecipePara other)
+        //{
+        //    var properties = GetType().GetProperties()
+        //       .Where(p => p.PropertyType != typeof(List<GlueConfig>));
+
+        //    foreach (var property in properties)
+        //    {
+        //        var value1 = property.GetValue(this);
+        //        var value2 = property.GetValue(other);
+        //        if (!Equals(value1, value2))
+        //        {
+        //            yield return $"{property.Name}: {value1} != {value2}";
+        //        }
+        //    }
+        //}
 
         private IEnumerable<string> CompareGlueConfigs(List<GlueConfig> otherConfigs)
         {
