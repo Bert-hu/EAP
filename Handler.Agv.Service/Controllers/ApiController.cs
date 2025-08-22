32404f983032d1814e4d366c8ef4c87e79dc322f
胡博 <bert_hu@usiglobal.com>
2025-08-22 15:26:34 +0800

    update log show

--- Handler.Agv.Service/Controllers/ApiController.cs
+++ Handler.Agv.Service/Controllers/ApiController.cs
@@ -134,11 +134,13 @@ namespace HandlerAgv.Service.Controllers
                             machine.InputTrayNumber = request.LotLayers ?? 0;
                             machine.CurrentLot = request.InputLot;
                             sqlSugarClient.Updateable(machine).UpdateColumns(it => new { it.InputTrayNumber }).ExecuteCommand();
+                            dbgLog.Info($"{machine.Id} 更新上料口盘数 {machine.InputTrayNumber}, 当前Lot {machine.CurrentLot}");
                         }
                         else if (task.Type == AgvTaskType.Output)
                         {
                             machine.OutputTrayNumber = 0;
                             sqlSugarClient.Updateable(machine).UpdateColumns(it => new { it.OutputTrayNumber }).ExecuteCommand();
+                            dbgLog.Info($"{machine.Id} 更新出料口盘数 0");
                         }
                         else if (task.Type == AgvTaskType.InputOutput)
                         {
@@ -146,6 +148,7 @@ namespace HandlerAgv.Service.Controllers
                             machine.CurrentLot = request.InputLot;
                             machine.OutputTrayNumber = 0;
                             sqlSugarClient.Updateable(machine).UpdateColumns(it => new { it.InputTrayNumber, it.OutputTrayNumber }).ExecuteCommand();
+                            dbgLog.Info($"{machine.Id} 更新上料口盘数 {machine.InputTrayNumber}, 当前Lot {machine.CurrentLot}， 出料口盘数 0");
                         }
 
                     }
