fd9ff51ad2e6866529c92261c9b04bc6c34021a9
Bo Hu <hubo611@foxmail.com>
2025-09-02 10:30:30 +0800

    machine页面新增库存信息统计

---
+++ Handler.Agv.Service/Models/Inventory/StockerInventory_I.cs
@@ -0,0 +1,10 @@
+﻿namespace HandlerAgv.Service.Models.Inventory
+{
+    [SqlSugar.SugarTable("USI_STOCK_A2F.HANDLE_LOT_LOCATION")]
+    public class StockerInventory_I
+    {
+        public string MODELNAME { get; set; }
+        public string GROUPNAME { get; set; }
+        public string STATUS { get; set; }
+    }
+}
